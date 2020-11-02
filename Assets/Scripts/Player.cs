756df0152c382286040991ba043ecaa4dc0940a1
OMGisDan <dan_rric@hotmail.com>
2020-11-02 20:21:11 -0300

    initial unity project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,9 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    // Start is called before the first frame update
+}
